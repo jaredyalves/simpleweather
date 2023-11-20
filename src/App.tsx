35ad393a832b0d769fbfd9a5ba64e64d9fc78049
Járedy Alves <94395541+jaredyalves@users.noreply.github.com>
2023-11-20 17:16:01 +0000

    Add a starter layout for showing weather info

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-function App() {
+import { useEffect, useState } from 'react';
+
+const App = () => {
+    const [query, setQuery] = useState('London, GB');
+
+    useEffect(() => {
+        const delay = setTimeout(() => {
+            console.log('query: ', query);
+        }, 2000);
+
+        return () => {
+            clearTimeout(delay);
+        };
+    }, [query]);
+
+    const handleInput = (e: React.FormEvent<HTMLSpanElement>) => {
+        if (e.currentTarget.textContent) {
+            setQuery(e.currentTarget.textContent);
+        }
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLSpanElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+        }
+    };
+
     return (
         <>
-            <div>Hello, world!</div>
+            <div className="flex h-screen flex-col items-center justify-center">
+                <div className="text-2xl text-neutral-400">
+                    Right now in&nbsp;
+                    <span
+                        autoFocus
+                        contentEditable
+                        onInput={handleInput}
+                        onKeyDown={handleKeyDown}
+                        className="border-b border-b-neutral-400 p-2 text-white focus:border-b-white"
+                        dangerouslySetInnerHTML={{ __html: 'London, GB' }}
+                    />
+                    , broken clouds.
+                </div>
+            </div>
         </>
     );
-}
+};
 
 export default App;
